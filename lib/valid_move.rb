83241808f31fc904a2563f7968f9dd3134f29c4d
Matt Ericksen <ericksenm93@ymail.com>
2020-07-10 18:42:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,12 @@
 # code your #valid_move? method here
 
 def valid_move?(board, ind)
-  if !(position_taken?(board, ind)) && ind.between?(0,8)
+  if !(position_taken?(board, ind)) && ind.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
     
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
